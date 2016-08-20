7e3c82439f0873f0c18bd7017be2e024ed83afc8
SamBarbee <sambarbee@me.com>
2016-08-20 01:54:14 -0500

    Initial Commit

---
+++ js/functions.js
@@ -0,0 +1,13 @@
+$(window).scroll(function(){
+
+  var wScroll = $(this).scrollTop();
+
+  $('.logo').css({
+    'transform' : 'translate(0px, '+ wScroll /6 +'%)'
+  });
+
+  $('.smoke').css({
+    'transform' : 'translate(0px, '+ wScroll /9 +'%)'
+  });
+
+});
