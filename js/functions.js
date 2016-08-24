e0fa1536077fc1e9ed148537985c7794a891847f
Sam Barbee <sambarbee@me.com>
2016-08-24 16:39:37 -0500

    img paralax

--- js/functions.js
+++ js/functions.js
@@ -3,7 +3,7 @@ $(window).scroll(function(){
   var wScroll = $(this).scrollTop();
 
   $('.logo').css({
-    'transform' : 'translate(0px, '+ wScroll /6 +'%)'
+    'transform' : 'translate(0px, '+ -wScroll /40 +'%)'
   });
 
   $('.smoke').css({
