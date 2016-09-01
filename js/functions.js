3f6ba6cad73b6f7e31978c3b77d2d9fdfa46ff2e
Sam Barbee <S376614@gisd.local>
2016-09-01 10:39:05 -0500

    more sections

--- js/functions.js
+++ js/functions.js
@@ -6,8 +6,14 @@ $(window).scroll(function(){
     'transform' : 'translate(0px, '+ -wScroll /40 +'%)'
   });
 
-  $('.smoke').css({
-    'transform' : 'translate(0px, '+ wScroll /4 +'%)'
-  });
+  if(wScroll > $('#about').offset().top - ($(window).height() / 1.2)) {
 
+    $('#about section').each(function(i){
+
+      setTimeout(function(){
+        $('#about section').eq(i).addClass('is-showing');
+      }, 150 * (i+1));
+    });
+
+  }
 });
