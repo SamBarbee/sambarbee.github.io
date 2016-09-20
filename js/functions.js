ad996fef97ae54f9d329ac561f106261fd14b75b
SamBarbee <sambarbee@me.com>
2016-09-20 09:52:44 -0500

    Section Animation

--- js/functions.js
+++ js/functions.js
@@ -6,13 +6,13 @@ $(window).scroll(function(){
     'transform' : 'translate(0px, '+ -wScroll /40 +'%)'
   });
 
-  if(wScroll > $('#about').offset().top - ($(window).height() / 1.2)) {
+  if(wScroll > $('#about').offset().top - ($(window).height() / 1)) {
 
     $('#about section').each(function(i){
 
       setTimeout(function(){
         $('#about section').eq(i).addClass('is-showing');
-      }, 150 * (i+1));
+      }, 1000 * (i+1));
     });
 
   }
